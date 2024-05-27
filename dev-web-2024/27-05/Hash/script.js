e07018cedcff7c77b482aee75e3c787c272c2939
ZaharoffG <118235912+ZaharoffG@users.noreply.github.com>
2024-05-27 16:13:13 -0300

    Add files via upload

---
+++ dev-web-2024/27-05/Hash/script.js
@@ -0,0 +1,43 @@
+const alvo_md5 = '32308b4088dde0d22ca761fb0a6c430f';
+const sal = 'abc';
+let login;
+//Senha é senha123
+//Usando algoritmo MD5
+
+document.getElementById('btn_login').onclick = () => {
+    let entrada = document.getElementById('senha_md5').value;
+    entrada += sal;
+    const mensagem = document.getElementById('mensagem');
+    if (hex_md5(entrada) === alvo_md5){
+        mensagem.innerHTML = '<h1>senha correta</h1>';
+        login = 1;
+        sessionStorage.setItem('login', login);
+        window.location.href='pagina2.html';
+    } else {
+        mensagem.innerHTML = '<h1>senha incorreta</h1>';
+        login = 0;
+        sessionStorage.setItem('login', login);
+        window.location.href='pagina2.html';
+    }
+}
+
+//Usando algoritmo SHA-1
+
+const alvo_sha = 'f19da5cea472af8efd20aeaa622182eef6b50940';
+
+document.getElementById('btn_login_sha').onclick = () => {
+    let entrada = document.getElementById('senha_sha').value;
+    entrada += sal;
+    const mensagem = document.getElementById('mensagem');
+    if (hex_sha1(entrada) === alvo_sha){
+        mensagem.innerHTML = '<h1>senha correta</h1>';
+        login = 1;
+        sessionStorage.setItem('login', login);
+        window.location.href='pagina2.html';
+    } else {
+        mensagem.innerHTML = '<h1>senha incorreta</h1>';
+        login = 0;
+        sessionStorage.setItem('login', login);
+        window.location.href='pagina2.html';
+    }
+}
---
+++ dev-web-2024/27-05/Modulos/script.js
@@ -0,0 +1,7 @@
+import { burra } from "./lib.js";
+
+burra(
+    document.body,
+    `<h1>Um cabeçalho</h1>
+    <p>Um exemplo de parágrafo</p>`
+)
